ce2e9bdfaf8c62cbad04d666c5ba10169466e318
Hoangcodezaq <101127326+Hoangcodezaq@users.noreply.github.com>
2022-03-08 18:52:42 +0700

    Add files via upload
    adc

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Quốc Tế Phụ Nữ';
+const giftUrl = '';
+const eventName = 'Chúc Mừng ngày 8/3';
+const titleCard = 'Thiệp gửi BeBap';
+const contentCard = 'nay 8/3, chúc Hằng ngày càng xinh đẹp và mong sao cho kì thi hsg sắp tới sẽ đạt được giải nhất (để có tiền chốt đơn keke) *lời chúc từ người bạn chuyên tin ';
+
+
+const giftImage = 'hang.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
